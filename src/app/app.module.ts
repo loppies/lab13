b2a9740f59c159d42c61b27a1111e11a14fee442
loppies <96416715+loppies@users.noreply.github.com>
2022-12-15 20:28:22 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,16 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { TaskComponent } from './task/task.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [FormsModule, CommonModule, AppRoutingModule, BrowserModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+  ],
   declarations: [AppComponent, TaskComponent, NotFoundComponent],
   bootstrap: [AppComponent],
 })
