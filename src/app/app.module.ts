08c1eef5e0cb811a5c2a68e25de904200ef4b82a
loppies <96416715+loppies@users.noreply.github.com>
2022-12-20 10:22:09 +0100

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { TaskComponent } from './task/task.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import pl from '@angular/common/locales/pl';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatLuxonDateModule } from '@angular/material-luxon-adapter';
+
+registerLocaleData(pl);
 
 @NgModule({
   imports: [
@@ -18,6 +27,12 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     HttpClientModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatLuxonDateModule,
   ],
   declarations: [AppComponent, TaskComponent, NotFoundComponent],
   bootstrap: [AppComponent],
